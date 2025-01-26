dc03cf52dca77c14b7a9424c33291f6874065b9c
jcatillo <jcatillo1121@gmail.com>
2025-01-26 21:21:09 +0800

    feat: Activity 1

---
+++ Atillo/Activity1/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "John Carl O. Atillo";
+int age = 20;
+string address = "Inayawan, Cebu City";
+string nickname = "JC";
+string gender = "Male";
+string yr = "BSCS 2A";
+string[] programmingSkill = {"C", "Java"};
+bool programmingExp = true;
+string role = "Programmer";
+
+
+Console.WriteLine("----------Demographics----------");
+Console.WriteLine($"Name:\t\t{name}");
+Console.WriteLine($"Age:\t\t{age}");
+Console.WriteLine($"Address:\t{address}");
+Console.WriteLine($"Nickname:\t{nickname}");
+Console.WriteLine($"Gender:\t\t{gender}");
+Console.WriteLine($"Yr:\t\t{yr}");
+Console.WriteLine($"Programming Skills:");
+for(int i = 0; i < programmingSkill.Length; i++){
+    Console.WriteLine($"\t\t-- {programmingSkill[i]}");
+}
+Console.WriteLine("Programming Exp: " + (programmingExp? "Yes" : "No"));
+Console.WriteLine($"Role:\t\t{role}");
